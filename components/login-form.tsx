b60accbfe466a36e4db2038fcc8f80dd2817a9f9
Mahdi Hussain <mahdi.hussain.work@gmail.com>
2025-10-22 23:33:25 +0600

    adding google and apple authentication button

--- components/login-form.tsx
+++ components/login-form.tsx
@@ -1,3 +1,4 @@
+"use client";
 import { Button } from "@/components/ui/button";
 import {
     Card,
@@ -14,11 +15,16 @@ import {
 } from "@/components/ui/field";
 import { Input } from "@/components/ui/input";
 import { cn } from "@/lib/utils";
+import GoogleIcon from "@/public/images/GoogleIcon";
+import { useState } from "react";
+import { FaApple, FaEye, FaEyeSlash } from "react-icons/fa";
 
 export function LoginForm({
     className,
     ...props
 }: React.ComponentProps<"div">) {
+    const [showPassword, setShowPassword] = useState(false);
+
     return (
         <div className={cn("flex flex-col gap-6", className)} {...props}>
             <Card>
@@ -41,7 +47,7 @@ export function LoginForm({
                                 />
                             </Field>
                             <Field>
-                                <div className="flex items-center">
+                                <div className="flex items-center ">
                                     <FieldLabel htmlFor="password">
                                         Password
                                     </FieldLabel>
@@ -52,13 +58,54 @@ export function LoginForm({
                                         Forgot your password?
                                     </a>
                                 </div>
-                                <Input id="password" type="password" required />
+                                <div className="relative">
+                                    <Input
+                                        id="password"
+                                        type={
+                                            showPassword ? "text" : "password"
+                                        }
+                                        required
+                                    />
+                                    <button
+                                        className="absolute top-[8px] right-[12px]"
+                                        type="button"
+                                        onClick={() =>
+                                            setShowPassword(!showPassword)
+                                        }
+                                    >
+                                        {showPassword ? (
+                                            <FaEyeSlash className="cursor-pointer text-gray-500" />
+                                        ) : (
+                                            <FaEye className="cursor-pointer text-gray-500" />
+                                        )}
+                                    </button>
+                                </div>
                             </Field>
                             <Field>
                                 <Button type="submit">Login</Button>
-                                <Button variant="outline" type="button">
-                                    Login with Google
-                                </Button>
+                                <div className="flex items-center w-full">
+                                    <div className="flex-grow h-px bg-gray-300" />
+                                    <span className="px-3 text-sm text-gray-500 whitespace-nowrap">
+                                        Or continue with
+                                    </span>
+                                    <div className="flex-grow h-px bg-gray-300" />
+                                </div>
+                                <div className="flex items-center justify-center gap-2">
+                                    <Button
+                                        variant="outline"
+                                        type="button"
+                                        className="w-1/2 cursor-pointer"
+                                    >
+                                        <GoogleIcon /> Google
+                                    </Button>
+                                    <Button
+                                        variant="outline"
+                                        type="button"
+                                        className="w-1/2 cursor-pointer"
+                                    >
+                                        <FaApple /> Apple
+                                    </Button>
+                                </div>
                                 <FieldDescription className="text-center">
                                     Don&apos;t have an account?{" "}
                                     <a href="/sign-up">Sign up</a>
--- components/signup-form.tsx
+++ components/signup-form.tsx
@@ -1,11 +1,6 @@
+"use client";
 import { Button } from "@/components/ui/button";
-import {
-    Card,
-    CardContent,
-    CardDescription,
-    CardHeader,
-    CardTitle,
-} from "@/components/ui/card";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import {
     Field,
     FieldDescription,
@@ -14,20 +9,46 @@ import {
 } from "@/components/ui/field";
 import { Input } from "@/components/ui/input";
 import { cn } from "@/lib/utils";
+import GoogleIcon from "@/public/images/GoogleIcon";
+import { useState } from "react";
+import { FaApple, FaEye, FaEyeSlash } from "react-icons/fa";
 
 export function SignupForm({
     className,
     ...props
 }: React.ComponentProps<"div">) {
+    const [showPassword, setShowPassword] = useState(false);
+    const [showConfirmPassword, setShowConfirmPassword] = useState(false);
+
     return (
         <div className={cn("flex flex-col gap-6", className)} {...props}>
             <Card>
                 <CardHeader>
-                    <CardTitle>Create an account</CardTitle>
-                    <CardDescription>
-                        Let&apos;s get started. Fill in the details below to
-                        create your account.
-                    </CardDescription>
+                    <CardTitle className="text-2xl font-bold">
+                        Create Account
+                    </CardTitle>
+
+                    <Button
+                        variant="outline"
+                        type="button"
+                        className="cursor-pointer mt-4"
+                    >
+                        <GoogleIcon /> Sign up with Google
+                    </Button>
+                    <Button
+                        variant="outline"
+                        type="button"
+                        className=" cursor-pointer mt-1"
+                    >
+                        <FaApple /> Sign up with Apple
+                    </Button>
+                    <div className="flex items-center w-full mt-2">
+                        <div className="flex-grow h-px bg-gray-300" />
+                        <span className="px-3 text-sm text-gray-500 whitespace-nowrap">
+                            Or continue with
+                        </span>
+                        <div className="flex-grow h-px bg-gray-300" />
+                    </div>
                 </CardHeader>
                 <CardContent>
                     <form>
@@ -58,7 +79,28 @@ export function SignupForm({
                                         Password
                                     </FieldLabel>
                                 </div>
-                                <Input id="password" type="password" required />
+                                <div className="relative">
+                                    <Input
+                                        id="password"
+                                        type={
+                                            showPassword ? "text" : "password"
+                                        }
+                                        required
+                                    />
+                                    <button
+                                        className="absolute top-[8px] right-[12px]"
+                                        type="button"
+                                        onClick={() =>
+                                            setShowPassword(!showPassword)
+                                        }
+                                    >
+                                        {showPassword ? (
+                                            <FaEyeSlash className="cursor-pointer text-gray-500" />
+                                        ) : (
+                                            <FaEye className="cursor-pointer text-gray-500" />
+                                        )}
+                                    </button>
+                                </div>
                             </Field>
                             <Field>
                                 <div className="flex items-center">
@@ -66,10 +108,37 @@ export function SignupForm({
                                         Confirm Password
                                     </FieldLabel>
                                 </div>
-                                <Input id="password" type="password" required />
+                                <div className="relative">
+                                    <Input
+                                        id="password"
+                                        type={
+                                            showConfirmPassword
+                                                ? "text"
+                                                : "password"
+                                        }
+                                        required
+                                    />
+                                    <button
+                                        className="absolute top-[8px] right-[12px]"
+                                        type="button"
+                                        onClick={() =>
+                                            setShowConfirmPassword(
+                                                !showConfirmPassword
+                                            )
+                                        }
+                                    >
+                                        {showConfirmPassword ? (
+                                            <FaEyeSlash className="cursor-pointer text-gray-500" />
+                                        ) : (
+                                            <FaEye className="cursor-pointer text-gray-500" />
+                                        )}
+                                    </button>
+                                </div>
                             </Field>
                             <Field>
-                                <Button type="submit">Login</Button>
+                                <Button type="submit" className="mt-2">
+                                    Login
+                                </Button>
 
                                 <FieldDescription className="text-center">
                                     Already have an account?{" "}
