1013bf4f992fcd32295fd634c924116aa1b05e9a
Mahdi Hussain <mahdi.hussain.work@gmail.com>
2025-11-02 23:50:55 +0600

    working on request a quote page

--- app/request-quote/page.tsx
+++ app/request-quote/page.tsx
@@ -1,8 +1,25 @@
+"use client";
 import Link from "next/link";
+import { useState } from "react";
 import { FaWhatsapp } from "react-icons/fa";
 import { philosopher } from "../fonts/fonts";
 
 const page = () => {
+    const [formData, setFormData] = useState({
+        service: "",
+        services: ["Simple Nikah", "Online Nikah", "Event Nikah", "Shahadah"],
+        name: "",
+        email: "",
+        city: "",
+        country: "",
+        date: "",
+        time: "",
+        venue: "",
+        contactNumber: "",
+        personalAddress: "",
+        additionalInfo: "",
+        details: "",
+    });
     return (
         <div>
             <div className="max-w-7xl mx-auto p-4 md:py-10">
@@ -24,6 +41,88 @@ const page = () => {
                     <FaWhatsapp className=" text-3xl text-green-500" />
                     <p className="font-semibold">For Quick Response</p>
                 </Link>
+
+                <form>
+                    <div className="space-y-4">
+                        <div className="w-full">
+                            <label className="block text-sm font-medium mb-1">
+                                Service
+                            </label>
+                            <select
+                                className="w-full border p-2 rounded"
+                                value={formData.service}
+                                onChange={(e) =>
+                                    setFormData({
+                                        ...formData,
+                                        service: e.target.value,
+                                    })
+                                }
+                            >
+                                <option value="" disabled>
+                                    Select a service
+                                </option>
+                                {/* {formData.services.map((s, i) => (
+                                    <option key={i} value={s}>
+                                        {s}
+                                    </option>
+                                ))} */}
+                            </select>
+                        </div>
+                        <div className="flex flex-col md:flex-row gap-2">
+                            <div className="w-full">
+                                <label className="block text-sm font-medium mb-1">
+                                    Date
+                                </label>
+                                <input
+                                    type="date"
+                                    placeholder="Full Name"
+                                    className="w-full border p-2 rounded"
+                                    // value={formData.date}
+                                    // onChange={(e) =>
+                                    //     setFormData({
+                                    //         ...formData,
+                                    //         date: e.target.value,
+                                    //     })
+                                    // }
+                                />
+                            </div>
+                            <div className="w-full">
+                                <label className="block text-sm font-medium mb-1">
+                                    Time
+                                </label>
+                                <input
+                                    type="time"
+                                    placeholder="Full Name"
+                                    className="w-full border p-2 rounded"
+                                    // value={formData.time}
+                                    // onChange={(e) =>
+                                    //     setFormData({
+                                    //         ...formData,
+                                    //         time: e.target.value,
+                                    //     })
+                                    // }
+                                />
+                            </div>
+                        </div>
+                        <div>
+                            <label className="block text-sm font-medium mb-1">
+                                Venue Address
+                            </label>
+                            <input
+                                type="text"
+                                placeholder="XXXX Hotel, Central London, Post Code XXXX, United Kingdom"
+                                className="w-full border p-2 rounded"
+                                // value={formData.venue}
+                                // onChange={(e) =>
+                                //     setFormData({
+                                //         ...formData,
+                                //         venue: e.target.value,
+                                //     })
+                                // }
+                            />
+                        </div>
+                    </div>
+                </form>
             </div>
         </div>
     );
